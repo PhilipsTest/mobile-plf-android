81e263e7f65dd5c8bb7ec3322584ac3654aa3771
faizal <faizal.shah@philips.com>
2019-07-12 15:20:26 +0530

    code optimise

--- Source/usr/Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/User.java
+++ Source/usr/Source/Library/RegistrationApi/src/main/java/com/philips/cdp/registration/User.java
@@ -617,22 +617,21 @@ public class User {
 
         if (RegistrationConfiguration.getInstance().isHSDPSkipLoginConfigurationAvailable()) {
             RLog.d(TAG, "authorizeHSDP:hsdpLogin: HSDP Flow = ");
-            HSDPLoginService HSDPLoginService = new HSDPLoginService(mContext);
+            HSDPLoginService hsdpLoginService = new HSDPLoginService(mContext);
             if (hsdpAuthenticationListener != null) {
                 RLog.d(TAG, "authorizeHSDP: with mTraditionalLoginHandler ");
-                inithsdpLoginService(hsdpAuthenticationListener, HSDPLoginService);
-
+                hsdpLoginService.hsdpLogin(getAccessToken(), getUserId(), hsdpAuthenticationListener);
             } else {
                 throw new RuntimeException("Please provide HSDPAuthentiationListner");
             }
         }
     }
 
-    private void inithsdpLoginService(HSDPAuthenticationListener hsdpAuthenticationListener, HSDPLoginService HSDPLoginService) {
-        if(RegistrationHelper.getInstance().isMobileFlow() && getEmail().equals("null")){
-            HSDPLoginService.hsdpLogin(getAccessToken(), getMobile(), hsdpAuthenticationListener);
-        }  else{
-            HSDPLoginService.hsdpLogin(getAccessToken(), getEmail(), hsdpAuthenticationListener);
+    private String getUserId() {
+        if (RegistrationHelper.getInstance().isMobileFlow() && getEmail().equals("null")) {
+            return getMobile();
+        } else {
+            return getEmail();
         }
     }
 
