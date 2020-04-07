573cda7c08d0fa172557ab4f76a8363e705f7a81
Ranjitha <ranjitha.hs@philips.com>
2020-04-07 11:29:17 +0530

    Chuck library changes

---
+++ Source/ail/Source/DemoUApp/src/main/java/com/philips/platform/aildemo/ChuckEnableActivity.java
@@ -0,0 +1,13 @@
+package com.philips.platform.aildemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChuckEnableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chuck_enable_activity);
+    }
+}
