66394d0a82d635c8c81cc8e1e52fc0feb56e00ad
pabitra <pabitrakumar.sahoo@philips.com>
2020-04-17 13:27:50 +0530

    Added classes and package for OrderDetail

---
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/platform/mec/screens/history/orderDetail/MECOrderDetailFragment.kt
@@ -0,0 +1,21 @@
+/*
+ *  Copyright (c) Koninklijke Philips N.V., 2020
+ *
+ *  * All rights are reserved. Reproduction or dissemination
+ *
+ *  * in whole or in part is prohibited without the prior written
+ *
+ *  * consent of the copyright holder.
+ *
+ *
+ */
+
+package com.philips.platform.mec.screens.history.orderDetail
+
+import com.philips.platform.mec.screens.MecBaseFragment
+
+class MECOrderDetailFragment : MecBaseFragment(){
+    override fun getFragmentTag(): String {
+        return "MECOrderDetailFragment"
+    }
+}
