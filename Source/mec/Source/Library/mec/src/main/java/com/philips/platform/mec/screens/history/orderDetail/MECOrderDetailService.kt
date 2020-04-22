76880d3629bc9673fbb033c7c6ad188771c4873d
Indrajit Kumar <indrajit.kumar@philips.com>
2020-04-22 19:19:22 +0530

    Order detail screen done

--- Source/mec/Source/Library/mec/src/main/java/com/philips/platform/mec/screens/history/orderDetail/MECOrderDetailService.kt
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/platform/mec/screens/history/orderDetail/MECOrderDetailService.kt
@@ -12,11 +12,55 @@
 
 package com.philips.platform.mec.screens.history.orderDetail
 
+import android.content.Context
 import com.philips.platform.ecs.model.orders.ECSOrderDetail
+import com.philips.platform.mec.R
+import com.philips.platform.mec.screens.shoppingCart.MECCartSummary
 
 class MECOrderDetailService {
 
     fun getProductSubcategory(ecsOrderDetail: ECSOrderDetail?): String? {
         return ecsOrderDetail?.entries?.get(0)?.product?.summary?.subcategory
+    }
+
+    fun addAppliedOrderPromotionsToCartSummaryList(orderDetail: ECSOrderDetail, cartSummaryList: MutableList<MECCartSummary>) {
+        var name: String
+        var price: String
+        // name should come instead of code
+        if (orderDetail.appliedOrderPromotions.size > 0) {
+            for (i in 0 until orderDetail.appliedOrderPromotions.size) {
+                name = if (orderDetail.appliedOrderPromotions[i].promotion.code == null) {
+                    " "
+                } else {
+                    orderDetail.appliedOrderPromotions[i].promotion.code
+                }
+                price = "-" + orderDetail.orderDiscounts.formattedValue
+                cartSummaryList.add(MECCartSummary(name, price))
+            }
+        }
+    }
+
+    fun addAppliedVoucherToCartSummaryList(orderDetail: ECSOrderDetail, cartSummaryList: MutableList<MECCartSummary>) {
+        var name: String
+        var price: String
+        for (i in 0 until orderDetail.appliedVouchers.size) {
+            name = if (orderDetail.appliedVouchers[i].code == null) {
+                " "
+            } else {
+                orderDetail.appliedVouchers[i].code
+            }
+            price = "-" + orderDetail.appliedVouchers?.get(i)?.appliedValue?.formattedValue
+            cartSummaryList.add(MECCartSummary(name, price))
+        }
+    }
+
+    fun addDeliveryCostToCartSummaryList(context: Context, orderDetail: ECSOrderDetail, cartSummaryList: MutableList<MECCartSummary>) {
+        val name: String
+        val price: String
+        if (orderDetail.deliveryCost != null) {
+            name = context.getString(R.string.mec_shipping_cost)
+            price = orderDetail.deliveryCost.formattedValue
+            cartSummaryList.add(MECCartSummary(name, price))
+        }
     }
 }
