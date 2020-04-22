484650e2cb67f6b49a0f6c7ff3284965f0735ca8
pabitra <pabitrakumar.sahoo@philips.com>
2020-04-22 14:32:58 +0530

    Test case for shopping cart and View Model

---
+++ Source/mec/Source/Library/mec/src/test/java/com/philips/platform/mec/screens/shoppingCart/ECSShoppingCartRepositoryTest.kt
@@ -0,0 +1,109 @@
+/*
+ *  Copyright (c) Koninklijke Philips N.V., 2020
+ *
+ *  * All rights are reserved. Reproduction or dissemination
+ *
+ *  * in whole or in part is prohibited without the prior written
+ *
+ *  * consent of the copyright holder.
+ *
+ *
+ */
+
+package com.philips.platform.mec.screens.shoppingCart
+
+import com.philips.platform.ecs.ECSServices
+import com.philips.platform.ecs.integration.ECSCallback
+import com.philips.platform.ecs.model.cart.ECSEntries
+import com.philips.platform.ecs.model.oauth.ECSOAuthData
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+import kotlin.test.assertNotNull
+
+@PrepareForTest(EcsShoppingCartViewModel::class,ECSShoppingCartCallback::class,ECSVoucherCallback::class)
+@RunWith(PowerMockRunner::class)
+class ECSShoppingCartRepositoryTest {
+
+    lateinit var ecsShoppingCartRepository: ECSShoppingCartRepository
+
+    @Mock
+    lateinit var ecsServicesMock: ECSServices
+
+    @Mock
+    lateinit var ecsShoppingCartViewModelMock: EcsShoppingCartViewModel
+
+    @Mock
+    lateinit var authCallbackMock : ECSCallback<ECSOAuthData, Exception>
+
+    @Mock
+    lateinit var ecsEntriesMock: ECSEntries
+    @Mock
+    lateinit var ecsShoppingCartCallbackMock : ECSShoppingCartCallback
+
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        ecsShoppingCartRepository = ECSShoppingCartRepository(ecsShoppingCartViewModelMock,ecsServicesMock)
+        ecsShoppingCartRepository.authCallBack = authCallbackMock
+        ecsShoppingCartRepository.ecsShoppingCartCallback = ecsShoppingCartCallbackMock
+    }
+
+    @Test
+    fun getAuthCallBack() {
+        assertNotNull(ecsShoppingCartRepository.authCallBack)
+    }
+
+    @Test
+    fun setAuthCallBack() {
+        ecsShoppingCartRepository.authCallBack = authCallbackMock
+        assertNotNull(ecsShoppingCartRepository.authCallBack)
+    }
+
+    @Test
+    fun fetchShoppingCart() {
+        //TODO
+        //ecsShoppingCartRepository.fetchShoppingCart()
+    }
+
+    @Test
+    fun updateShoppingCart() {
+        ecsShoppingCartRepository.updateShoppingCart(ecsEntriesMock, 1)
+        Mockito.verify(ecsServicesMock).updateShoppingCart( 1,ecsEntriesMock,ecsShoppingCartCallbackMock)
+    }
+
+    @Test
+    fun fetchProductReview() {
+        //TODO
+       // ecsShoppingCartRepository.fetchProductReview()
+    }
+
+    @Mock
+    lateinit var ecsVoucherCallback: ECSVoucherCallback
+
+    @Test
+    fun applyVoucher() {
+        ecsShoppingCartRepository.applyVoucher("1234",ecsVoucherCallback)
+        Mockito.verify(ecsServicesMock).applyVoucher("1234",ecsVoucherCallback)
+    }
+
+    @Test
+    fun removeVoucher() {
+        ecsShoppingCartRepository.removeVoucher("1234",ecsVoucherCallback)
+        Mockito.verify(ecsServicesMock).removeVoucher("1234",ecsVoucherCallback)
+    }
+
+    @Test
+    fun createCart() {
+        ecsShoppingCartRepository.createCart(ecsShoppingCartCallbackMock)
+        Mockito.verify(ecsServicesMock).createShoppingCart(ecsShoppingCartCallbackMock)
+    }
+
+
+}
