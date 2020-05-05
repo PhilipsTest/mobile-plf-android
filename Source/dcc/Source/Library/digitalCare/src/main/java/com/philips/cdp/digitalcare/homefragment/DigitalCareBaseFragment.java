d27832c0fd6542ef2214c46e16fd5e8a7bd12672
Anurag Gautam <anurag.gautam@philips.com>
2020-05-05 12:20:31 +0530

    INC1790783

--- Source/dcc/Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/homefragment/DigitalCareBaseFragment.java
+++ Source/dcc/Source/Library/digitalCare/src/main/java/com/philips/cdp/digitalcare/homefragment/DigitalCareBaseFragment.java
@@ -139,7 +139,6 @@ public abstract class DigitalCareBaseFragment extends Fragment implements
         });
 
         webView.setWebChromeClient(new WebChromeClient() {
-            Bitmap videoPoster = BitmapFactory.decodeResource(getResources(), R.drawable.ic_media_video_poster);
 
             @Override
             public void onProgressChanged(WebView view, int newProgress) {
