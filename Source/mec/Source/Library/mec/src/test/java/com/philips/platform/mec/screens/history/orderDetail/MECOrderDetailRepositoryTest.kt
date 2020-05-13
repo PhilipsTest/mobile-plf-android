f157f529f12ba5bf3d0ad20be1a1a725f764dd14
Anurag Gautam <anurag.gautam@philips.com>
2020-05-13 14:37:05 +0530

    test case.

--- Source/mec/Source/Library/mec/src/test/java/com/philips/platform/mec/screens/history/orderDetail/MECOrderDetailRepositoryTest.kt
+++ Source/mec/Source/Library/mec/src/test/java/com/philips/platform/mec/screens/history/orderDetail/MECOrderDetailRepositoryTest.kt
@@ -1,11 +1,19 @@
 package com.philips.platform.mec.screens.history.orderDetail
 
+import android.content.Context
+import com.philips.cdp.prxclient.PRXDependencies
+import com.philips.cdp.prxclient.RequestManager
+import com.philips.cdp.prxclient.request.CustomerCareContactsRequest
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.powermock.core.classloader.annotations.PrepareForTest
 import org.powermock.modules.junit4.PowerMockRunner
 
+@PrepareForTest(CustomerCareContactsRequest::class, RequestManager::class, PRXDependencies::class)
 @RunWith(PowerMockRunner::class)
 class MECOrderDetailRepositoryTest {
 
@@ -15,11 +23,26 @@ class MECOrderDetailRepositoryTest {
     lateinit var  mPRXContactsResponseCallback: PRXContactsResponseCallback
 
 
+    @Mock
+    lateinit var mContext : Context
+
+    @Mock
+    lateinit var mCustomerCareContactsRequest :CustomerCareContactsRequest
+
+    @Mock
+     var mRequestManager =RequestManager()
+
+
+
     @Before
     fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        mECOrderDetailRepository=MECOrderDetailRepository()
     }
 
     @Test
     fun fetchContacts() {
+       mECOrderDetailRepository.fetchContacts(mContext,"",mPRXContactsResponseCallback)
+       Mockito.verify( mRequestManager).executeRequest(mCustomerCareContactsRequest,mPRXContactsResponseCallback)
     }
 }
