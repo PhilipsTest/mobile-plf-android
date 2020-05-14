05a33adb16dd2bd38b99183f98df329e3754d4a1
Anurag Gautam <anurag.gautam@philips.com>
2020-05-14 16:31:30 +0530

    review comments

--- Source/mec/Source/Library/mec/src/main/java/com/philips/platform/mec/integration/MECFlowConfigurator.kt
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/platform/mec/integration/MECFlowConfigurator.kt
@@ -12,10 +12,15 @@ package com.philips.platform.mec.integration
 import java.io.Serializable
 import java.util.*
 
-/**
- * MECFlowConfigurato initializes the required ctn’s for MEC to set the flow of micro app.
- * @since 2001.0
- */
+    /**
+    * MECFlowConfigurato initializes the flow for different MEC entry points.
+    MEC_PRODUCT_LIST_VIEW,                //landing view is used to launch the Product Catalogue Screen.
+    MEC_PRODUCT_DETAILS_VIEW,             //landing view is used to launch the Product Details view for the Product CTN passed
+    MEC_CATEGORIZED_PRODUCT_LIST_VIEW,    //landing view is used to launch the Product Catalogue Screen with a list of Product CTNs passed.
+    MEC_SHOPPING_CART_VIEW,               //landing view is used to launch the shopping cart view
+    MEC_ORDER_HISTORY                     //landing view is used to launch the order history view of an user
+     * @since 2001.0
+    */
 class MECFlowConfigurator : Serializable {
 
 
