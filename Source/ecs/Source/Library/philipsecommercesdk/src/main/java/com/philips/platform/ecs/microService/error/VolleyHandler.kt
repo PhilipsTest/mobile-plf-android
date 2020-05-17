84318a72a47aec3f1a2fad5c20ec8709ff7a8955
pabitra <pabitrakumar.sahoo@philips.com>
2020-05-17 09:56:00 +0530

    Handled volley error

--- Source/ecs/Source/Library/philipsecommercesdk/src/main/java/com/philips/platform/ecs/microService/error/VolleyHandler.kt
+++ Source/ecs/Source/Library/philipsecommercesdk/src/main/java/com/philips/platform/ecs/microService/error/VolleyHandler.kt
@@ -33,38 +33,45 @@ class VolleyHandler {
 
             is NoConnectionError -> ecsError = ecsVolleyError
             is TimeoutError -> ecsError = ecsVolleyError
-            is AuthFailureError -> {
-
-            }
+            is AuthFailureError -> { handleAuthError()}
             is NetworkError -> ecsError = ecsVolleyError
             is ParseError -> ecsError = ecsVolleyError
-            is ServerError ->{
-
-                ecsError = ECSError(ECSErrorType.ECSsomethingWentWrong.getLocalizedErrorString(),ECSErrorType.ECSsomethingWentWrong.errorCode,ECSErrorType.ECSsomethingWentWrong)
-                val jsonErrorObject = volleyError.getJsonError()
-                //For PIL
-                val pilError = jsonErrorObject?.getData(HybrisError::class.java)
-                if(pilError?.errors?.size ?:0 > 0){
-                    setPILECSError(pilError, ecsError)
-                }
-
-                //for OCC Hybris config TODO - to be removed
-                val occError = jsonErrorObject?.getData(OCCServerError::class.java)
-                if(occError?.errors?.size ?:0 > 0){
-
-                    val localizedStringID = occError?.errors?.get(0)?.type
-                    localizedStringID?.let {
-                        val ecsErrorType = ECSErrorType.valueOf("ECS$localizedStringID")
-                        ecsError = ECSError(ecsErrorType.getLocalizedErrorString(), ecsErrorType.errorCode, ecsErrorType)
-                    }
-                }
-
-            }
+            is ServerError ->{ handleServerError(volleyError, ecsError) }
 
         }
-
-
         return ecsError
+    }
+
+    private fun handleAuthError() {
+
+    }
+
+    private fun handleServerError(volleyError: ServerError, ecsError: ECSError) {
+
+        val jsonErrorObject = volleyError.getJsonError()
+
+        //For PIL
+        val pilError = jsonErrorObject?.getData(HybrisError::class.java)
+        if (pilError?.errors?.size ?: 0 > 0) {
+            setPILECSError(pilError, ecsError)
+        }
+
+        //for OCC Hybris config TODO - to be removed
+        val occError = jsonErrorObject?.getData(OCCServerError::class.java)
+        if (occError?.errors?.size ?: 0 > 0) {
+
+            val localizedStringID = occError?.errors?.get(0)?.type
+            localizedStringID?.let {
+                val ecsErrorType = ECSErrorType.valueOf("ECS$localizedStringID")
+                setEcsError(ecsError, ecsErrorType)
+            }
+        }
+    }
+
+    private fun setEcsError(ecsError: ECSError, ecsErrorType: ECSErrorType) {
+        ecsError.errorMessage = ecsErrorType.getLocalizedErrorString()
+        ecsError.errorcode = ecsErrorType.errorCode
+        ecsError.errorType = ecsErrorType
     }
 
     private fun setPILECSError(pilError: HybrisError?, ecsError: ECSError) {
@@ -76,9 +83,7 @@ class VolleyHandler {
 
             val localizedStringID = "ECS$firstFailureString"
             val ecsErrorType = ECSErrorType.valueOf(localizedStringID)
-            ecsError.errorMessage = ecsErrorType.getLocalizedErrorString()
-            ecsError.errorcode= ecsErrorType.errorCode
-            ecsError.errorType = ecsErrorType
+            setEcsError(ecsError, ecsErrorType)
         }
 
     }
