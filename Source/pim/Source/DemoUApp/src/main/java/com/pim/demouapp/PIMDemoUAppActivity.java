8ea62bb1913d431003b9c44c6989b9ce74f4b462
Shashi Ranjan <shashi.ranjan@philips.com>
2020-05-27 12:44:32 +0530

    Enabled progress dialog for logout request

--- Source/pim/Source/DemoUApp/src/main/java/com/pim/demouapp/PIMDemoUAppActivity.java
+++ Source/pim/Source/DemoUApp/src/main/java/com/pim/demouapp/PIMDemoUAppActivity.java
@@ -68,6 +68,7 @@ import com.philips.platform.pif.DataInterface.USR.listeners.RefetchUserDetailsLi
 import com.philips.platform.pif.DataInterface.USR.listeners.RefreshSessionListener;
 import com.philips.platform.pif.DataInterface.USR.listeners.UpdateUserDetailsHandler;
 import com.philips.platform.pim.UDIRedirectReceiverActivity;
+import com.philips.platform.pim.errors.PIMErrorCodes;
 import com.philips.platform.pim.listeners.UserLoginListener;
 import com.philips.platform.pim.listeners.UserMigrationListener;
 import com.philips.platform.pim.PIMInterface;
@@ -377,15 +378,18 @@ public class PIMDemoUAppActivity extends AppCompatActivity implements View.OnCli
             }
         } else if (v == btnLogout) {
             if (isUserLoggedIn()) {
+                showProgressDialog();
                 userDataInterface.logoutSession(new LogoutSessionListener() {
                     @Override
                     public void logoutSessionSuccess() {
+                        cancelProgressDialog();
                         showToast("Logout Success");
                         finish();
                     }
 
                     @Override
                     public void logoutSessionFailed(Error error) {
+                        cancelProgressDialog();
                         showToast("Logout Failed with error code " + error.getErrCode());
                     }
                 });
@@ -488,7 +492,10 @@ public class PIMDemoUAppActivity extends AppCompatActivity implements View.OnCli
                     @Override
                     public void onRefetchFailure(Error error) {
                         cancelProgressDialog();
-                        showToast("Refetch failed with error code : " + error.getErrCode());
+                        if (error != null && error.getErrCode() == PIMErrorCodes.ACCESS_TOKEN_EXPIRED)
+                            showToast("Refetch failed due to due to expired access token.");
+                        else
+                            showToast("Refetch failed with error code : " + error.getErrCode());
                     }
                 });
             } else {
@@ -554,7 +561,10 @@ public class PIMDemoUAppActivity extends AppCompatActivity implements View.OnCli
                 @Override
                 public void onUpdateFailedWithError(Error error) {
                     cancelProgressDialog();
-                    showToast("Updating marketing opted-in failed with error code : " + error.getErrCode());
+                    if (error != null && error.getErrCode() == PIMErrorCodes.ACCESS_TOKEN_EXPIRED)
+                        showToast("Updating marketing opted-in failed due to expired access token.");
+                    else
+                        showToast("Updating marketing opted-in failed with error code : " + error.getErrCode());
                     updateMarketingOptinStatus();
                 }
             }, marketingOptedSwitch.isChecked());
@@ -714,7 +724,7 @@ public class PIMDemoUAppActivity extends AppCompatActivity implements View.OnCli
         android.widget.Button button = alertDialog.getButton(AlertDialog.BUTTON_NEUTRAL);
         ViewGroup.LayoutParams params = button.getLayoutParams();
         params.width = ViewGroup.LayoutParams.MATCH_PARENT;
-        button.setBackgroundColor(getResources().getColor(R.color.uid_blue_level_50,null));
+        button.setBackgroundColor(getResources().getColor(R.color.uid_blue_level_50, null));
         button.setTextColor(getResources().getColor(R.color.uidColorWhite, null));
         button.setTextSize(18);
         button.setLayoutParams(params);
