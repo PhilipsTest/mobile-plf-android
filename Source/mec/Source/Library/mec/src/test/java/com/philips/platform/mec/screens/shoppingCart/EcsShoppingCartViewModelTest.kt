7db11835ecc5fbcf7fb5982b403a9af20c9d52cb
Anurag Gautam <anurag.gautam@philips.com>
2020-06-16 20:14:48 +0530

    test case

--- Source/mec/Source/Library/mec/src/test/java/com/philips/platform/mec/screens/shoppingCart/EcsShoppingCartViewModelTest.kt
+++ Source/mec/Source/Library/mec/src/test/java/com/philips/platform/mec/screens/shoppingCart/EcsShoppingCartViewModelTest.kt
@@ -12,8 +12,14 @@
 
 package com.philips.platform.mec.screens.shoppingCart
 
+import com.philips.platform.appinfra.AppInfraInterface
 import com.philips.platform.ecs.ECSServices
 import com.philips.platform.ecs.integration.ECSCallback
+import com.philips.platform.ecs.model.cart.BasePriceEntity
+import com.philips.platform.ecs.model.cart.ECSEntries
+import com.philips.platform.ecs.model.products.ECSProduct
+import com.philips.platform.ecs.model.products.PriceEntity
+import com.philips.platform.mec.common.MECRequestType
 import com.philips.platform.mec.utils.MECDataHolder
 import org.junit.After
 import org.junit.Before
@@ -27,11 +33,14 @@ import org.powermock.core.classloader.annotations.PrepareForTest
 import org.powermock.modules.junit4.PowerMockRunner
 
 
-@PrepareForTest(ECSShoppingCartRepository::class,ECSShoppingCartCallback::class,ECSVoucherCallback::class,ECSCallback::class)
+@PrepareForTest(ECSShoppingCartRepository::class, ECSShoppingCartCallback::class, ECSVoucherCallback::class, ECSCallback::class)
 @RunWith(PowerMockRunner::class)
 class EcsShoppingCartViewModelTest {
 
     lateinit var ecsShoppingCartViewModel: EcsShoppingCartViewModel
+
+    @Mock
+    lateinit var appInfraMock: AppInfraInterface
 
     @Mock
     lateinit var ecsShoppingCartRepositoryMock: ECSShoppingCartRepository
@@ -46,7 +55,7 @@ class EcsShoppingCartViewModelTest {
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
-
+        MECDataHolder.INSTANCE.appinfra=appInfraMock
         MECDataHolder.INSTANCE.eCSServices = ecsServicesMock
         ecsShoppingCartRepositoryMock.ecsServices = ecsServicesMock
         ecsShoppingCartRepositoryMock.ecsShoppingCartCallback = ecsShoppingCartCallbackMock
@@ -62,11 +71,61 @@ class EcsShoppingCartViewModelTest {
     fun testCreateShoppingCart() {
 
         ecsShoppingCartViewModel.createShoppingCart("")
-      //  Mockito.verify(ecsServicesMock).createShoppingCart(ArgumentMatchers.any(ECSCallback::class.java) as ECSCallback<ECSShoppingCart, Exception>)
-        Mockito.verify(ecsServicesMock, Mockito.atLeastOnce()).createShoppingCart(ArgumentMatchers.any())
-       // Mockito.verify(ecsShoppingCartRepositoryMock).createCart(ecsShoppingCartCallbackMock)
+        //  Mockito.verify(ecsServicesMock).createShoppingCart(ArgumentMatchers.any(ECSCallback::class.java) as ECSCallback<ECSShoppingCart, Exception>)
+        Mockito.verify(ecsServicesMock, Mockito.atLeastOnce())
+                .createShoppingCart(ArgumentMatchers.any())
+        // Mockito.verify(ecsShoppingCartRepositoryMock, Mockito.atLeastOnce()).createCart(ArgumentMatchers.any())
     }
 
+
+    @Test
+    fun testGetShoppingCart() {
+
+//        Mockito.`when`(MECutility.isExistingUser()) .thenReturn(true)
+        ecsShoppingCartViewModel.getShoppingCart()
+
+       // Mockito.verify(ecsServicesMock, Mockito.atLeastOnce()).fetchShoppingCart(ArgumentMatchers.any())
+       //  Mockito.verify(ecsShoppingCartRepositoryMock, Mockito.atLeastOnce()).fetchShoppingCart()
+
+    }
+
+
+    @Test
+    fun TestUpdateQuantity() {
+        val map = HashMap<String, String>()
+        map.put("key1", "value1")
+
+        var eCSentry = ECSEntries()
+        var mECSProduct = ECSProduct()
+        mECSProduct.code = "ConsignmentCode123ABC"
+        var priceEntity = PriceEntity()
+        priceEntity.value = 12.9
+        mECSProduct.price = priceEntity
+
+        eCSentry.product = mECSProduct
+        eCSentry.quantity = 2
+
+        var basePriceEntity = BasePriceEntity()
+        basePriceEntity.value = 10.7
+        eCSentry.basePrice = basePriceEntity
+
+
+        ecsShoppingCartViewModel.updateQuantity(eCSentry, 3)
+        Mockito.verify(ecsServicesMock, Mockito.atLeastOnce()).updateShoppingCart(ArgumentMatchers.anyInt(), ArgumentMatchers.anyObject(), ArgumentMatchers.any())
+
+    }
+
+    @Test
+    fun TestAddVoucher(){
+        ecsShoppingCartViewModel.addVoucher("",MECRequestType.MEC_APPLY_VOUCHER)
+        Mockito.verify(ecsServicesMock, Mockito.atLeastOnce()).applyVoucher(ArgumentMatchers.anyString(),ArgumentMatchers.any())
+    }
+
+    @Test
+    fun TestRemoveVoucher(){
+        ecsShoppingCartViewModel.removeVoucher("")
+        Mockito.verify(ecsServicesMock, Mockito.atLeastOnce()).removeVoucher(ArgumentMatchers.anyString(),ArgumentMatchers.any())
+    }
 
 
     @After
