6f2199685563e1c41942d68b0444e7f14e2344bb
pabitra <pabitrakumar.sahoo@philips.com>
2020-06-23 15:59:00 +0530

    Added ECSCartManager

--- Source/ecs/Source/Library/philipsecommercesdk/src/main/java/com/philips/platform/ecs/microService/manager/ECSCartManager.kt
+++ Source/ecs/Source/Library/philipsecommercesdk/src/main/java/com/philips/platform/ecs/microService/manager/ECSCartManager.kt
@@ -12,5 +12,19 @@
 
 package com.philips.platform.ecs.microService.manager
 
+import com.philips.platform.ecs.microService.callBack.ECSCallback
+import com.philips.platform.ecs.microService.error.ECSError
+import com.philips.platform.ecs.model.cart.ECSShoppingCart
+
 class ECSCartManager {
+
+    val requestHandler = RequestHandler()
+
+    fun fetchShoppingCart(ecsCallback: ECSCallback<ECSShoppingCart, ECSError>){
+        val ecsException = ECSApiValidator().getECSException(APIType.LocaleHybrisAndAuth)
+
+        ecsException?.let { throw ecsException } ?: kotlin.run {
+
+        }
+    }
 }
