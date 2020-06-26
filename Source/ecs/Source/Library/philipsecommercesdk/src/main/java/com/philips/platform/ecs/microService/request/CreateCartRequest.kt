b842af6ce16454304f5e0abd6120e1849f3cf933
Anurag Gautam <anurag.gautam@philips.com>
2020-06-26 14:31:35 +0530

    removed product detail

--- Source/ecs/Source/Library/philipsecommercesdk/src/main/java/com/philips/platform/ecs/microService/request/CreateCartRequest.kt
+++ Source/ecs/Source/Library/philipsecommercesdk/src/main/java/com/philips/platform/ecs/microService/request/CreateCartRequest.kt
@@ -4,9 +4,7 @@ import com.philips.platform.ecs.microService.callBack.ECSCallback
 import com.philips.platform.ecs.microService.constant.ECSConstants.Companion.SERVICEID_ECS_CREATE_CART
 import com.philips.platform.ecs.microService.error.ECSError
 import com.philips.platform.ecs.microService.error.ErrorHandler
-import com.philips.platform.ecs.microService.manager.ECSProductManager
 import com.philips.platform.ecs.microService.model.cart.ECSPILShoppingCart
-import com.philips.platform.ecs.microService.model.product.ECSProduct
 import com.philips.platform.ecs.microService.util.getData
 import com.philips.platform.ecs.microService.util.replaceParam
 import org.json.JSONObject
@@ -36,24 +34,9 @@ class CreateCartRequest(private val ctn: String, private val quantity: Int,priva
     override fun onResponse(response: JSONObject) {
         val ecsShoppingCart = response.getData(ECSPILShoppingCart::class.java)
         
-        ecsShoppingCart ?.let { getCartProductDetails(ecsShoppingCart)  } ?: kotlin.run {  ecsCallback.onFailure( ErrorHandler().getECSError(null))}
+        ecsShoppingCart ?.let { ecsCallback.onResponse(ecsShoppingCart)  } ?: kotlin.run {  ecsCallback.onFailure( ErrorHandler().getECSError(null))}
 
     }
 
-    private fun getCartProductDetails(ecsShoppingCart: ECSPILShoppingCart) {
-        for (item in ecsShoppingCart.data.attributes.items){
-            ECSProductManager().getProductFor(item.id,object : ECSCallback<ECSProduct?, ECSError>{
-                override fun onResponse(result: ECSProduct?) {
-                    TODO("Not yet implemented")
-                }
 
-                override fun onFailure(ecsError: ECSError) {
-                    TODO("Not yet implemented")
-                }
-            })
-        }
-
-        //once all products are fetched for ID , give back success callback
-        ecsCallback.onResponse(ecsShoppingCart)
-    }
 }
