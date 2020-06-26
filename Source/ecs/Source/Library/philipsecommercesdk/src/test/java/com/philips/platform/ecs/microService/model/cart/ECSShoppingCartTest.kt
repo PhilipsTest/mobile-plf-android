66391d33a6e5750daa91b58af433344f16a608bc
pabitra <pabitrakumar.sahoo@philips.com>
2020-06-26 23:09:12 +0530

    Adding test cases

---
+++ Source/ecs/Source/Library/philipsecommercesdk/src/test/java/com/philips/platform/ecs/microService/model/cart/ECSShoppingCartTest.kt
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (c) Koninklijke Philips N.V., 2020
+ *
+ *  * All rights are reserved. Reproduction or dissemination
+ *
+ *  * in whole or in part is prohibited without the prior written
+ *
+ *  * consent of the copyright holder.
+ *
+ *
+ */
+
+package com.philips.platform.ecs.microService.model.cart
+
+import org.json.JSONObject
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.MockitoAnnotations
+import org.powermock.modules.junit4.PowerMockRunner
+
+@RunWith(PowerMockRunner::class)
+class ECSShoppingCartTest {
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun `test shopping cart json parsing`() {
+        val errorString =   ClassLoader.getSystemResource("pil/cart/AddToCartSuccessWithProductMoreQuantityThanStock.json").readText()
+        val jsonObject = JSONObject(errorString)
+
+        jsonObject.
+    }
+}
---
+++ Source/ecs/Source/Library/philipsecommercesdk/src/test/java/com/philips/platform/ecs/microService/request/GetCartRequestTest.kt
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (c) Koninklijke Philips N.V., 2020
+ *
+ *  * All rights are reserved. Reproduction or dissemination
+ *
+ *  * in whole or in part is prohibited without the prior written
+ *
+ *  * consent of the copyright holder.
+ *
+ *
+ */
+
+package com.philips.platform.ecs.microService.request
+
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.powermock.modules.junit4.PowerMockRunner
+
+@RunWith(PowerMockRunner::class)
+class GetCartRequestTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getHardCodedCartURL() {
+    }
+
+    @Test
+    fun getServiceID() {
+    }
+
+    @Test
+    fun getURL() {
+    }
+
+    @Test
+    fun getReplaceURLMap() {
+    }
+
+    @Test
+    fun onResponse() {
+    }
+
+    @Test
+    fun getEcsCallback() {
+    }
+}
