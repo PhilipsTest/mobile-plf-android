b61eb831a3c74f6f1c84c180e25a7b0dd2680fde
Anurag Gautam <anurag.gautam@philips.com>
2020-07-02 14:44:40 +0530

    test case

--- Source/ecs/Source/Library/philipsecommercesdk/src/test/java/com/philips/platform/ecs/microService/manager/ECSCartManagerTest.kt
+++ Source/ecs/Source/Library/philipsecommercesdk/src/test/java/com/philips/platform/ecs/microService/manager/ECSCartManagerTest.kt
@@ -4,6 +4,8 @@ import com.philips.platform.appinfra.AppInfra
 import com.philips.platform.appinfra.appconfiguration.AppConfigurationInterface
 import com.philips.platform.ecs.microService.callBack.ECSCallback
 import com.philips.platform.ecs.microService.error.ECSError
+import com.philips.platform.ecs.microService.error.ECSErrorType
+import com.philips.platform.ecs.microService.error.ECSException
 import com.philips.platform.ecs.microService.model.cart.Data
 import com.philips.platform.ecs.microService.model.cart.ECSShoppingCart
 import com.philips.platform.ecs.microService.model.config.ECSConfig
@@ -11,6 +13,7 @@ import com.philips.platform.ecs.microService.request.CreateCartRequest
 import com.philips.platform.ecs.microService.request.any
 import com.philips.platform.ecs.microService.util.ECSDataHolder
 import junit.framework.Assert
+import junit.framework.Assert.assertEquals
 import junit.framework.Assert.fail
 import org.junit.Before
 import org.junit.Test
@@ -30,31 +33,35 @@ class ECSCartManagerTest {
     lateinit var requestHandlerMock: RequestHandler
 
     @Mock
-    lateinit var CreateCartRequestMock:  CreateCartRequest
+    lateinit var CreateCartRequestMock: CreateCartRequest
 
     @Mock
-    lateinit var appConfigurationInterfaceMock : AppConfigurationInterface
+    lateinit var appConfigurationInterfaceMock: AppConfigurationInterface
 
     @Mock
-    lateinit var appInfraMock : AppInfra
+    lateinit var appInfraMock: AppInfra
 
     @Before
     fun setUp() {
         ECSDataHolder.locale = "en_US"
-        ECSDataHolder.authToken="AUTH"
+        ECSDataHolder.authToken = "AUTH"
         mECSCartManager = ECSCartManager()
-       mECSCartManager.requestHandler=requestHandlerMock
+        mECSCartManager.requestHandler = requestHandlerMock
     }
 
     private fun setApiKey() {
-        Mockito.`when`(appConfigurationInterfaceMock.getPropertyForKey(any(String::class.java), any(String::class.java), any(AppConfigurationInterface.AppConfigurationError::class.java))).thenReturn("yaTmSAVqDR4GNwijaJie3aEa3ivy7Czu22BxZwKP")
-        Mockito.`when`(appInfraMock.configInterface).thenReturn(appConfigurationInterfaceMock)
+        Mockito.`when`(appConfigurationInterfaceMock.getPropertyForKey(any(String::class.java), any(String::class.java), any(AppConfigurationInterface.AppConfigurationError::class.java)))
+                .thenReturn("yaTmSAVqDR4GNwijaJie3aEa3ivy7Czu22BxZwKP")
+        Mockito.`when`(appInfraMock.configInterface)
+                .thenReturn(appConfigurationInterfaceMock)
         ECSDataHolder.appInfra = appInfraMock
     }
 
     private fun setApiKeyNull() {
-        Mockito.`when`(appConfigurationInterfaceMock.getPropertyForKey(any(String::class.java), any(String::class.java), any(AppConfigurationInterface.AppConfigurationError::class.java))).thenReturn(null)
-        Mockito.`when`(appInfraMock.configInterface).thenReturn(appConfigurationInterfaceMock)
+        Mockito.`when`(appConfigurationInterfaceMock.getPropertyForKey(any(String::class.java), any(String::class.java), any(AppConfigurationInterface.AppConfigurationError::class.java)))
+                .thenReturn(null)
+        Mockito.`when`(appInfraMock.configInterface)
+                .thenReturn(appConfigurationInterfaceMock)
         ECSDataHolder.appInfra = appInfraMock
     }
 
@@ -68,30 +75,112 @@ class ECSCartManagerTest {
         ECSDataHolder.config = ECSConfig(isHybris = true)
         setApiKey()
 
-        var ecscallback = object: ECSCallback<ECSShoppingCart, ECSError> {
+        var ecscallback = object : ECSCallback<ECSShoppingCart, ECSError> {
             override fun onResponse(result: ECSShoppingCart) {
                 Assert.assertNotNull(result)
             }
+
             override fun onFailure(ecsError: ECSError) {
-             fail()
+                fail()
             }
         };
 
-        var data= Data(null,"id","type")
+        var data = Data(null, "id", "type")
         var eCSShoppingCart = ECSShoppingCart(data)
-        Mockito.`when`(requestHandlerMock.handleRequest(CreateCartRequestMock)).then { ecscallback.onResponse(eCSShoppingCart) }
-        CreateCartRequestMock = CreateCartRequest("ctn",1,ecscallback)
-        mECSCartManager.createECSShoppingCart("ctn",1,ecscallback)
-        Mockito.`when`(requestHandlerMock.handleRequest(CreateCartRequestMock)).then { ecscallback.onResponse(eCSShoppingCart) }
+        Mockito.`when`(requestHandlerMock.handleRequest(CreateCartRequestMock))
+                .then { ecscallback.onResponse(eCSShoppingCart) }
+        CreateCartRequestMock = CreateCartRequest("ctn", 1, ecscallback)
+        mECSCartManager.createECSShoppingCart("ctn", 1, ecscallback)
+        Mockito.`when`(requestHandlerMock.handleRequest(CreateCartRequestMock))
+                .then { ecscallback.onResponse(eCSShoppingCart) }
     }
 
     @Test
-    fun `invalid ctn`(){
+    fun `invalid ctn`() {
+        ECSDataHolder.locale = "en_US"
+        ECSDataHolder.config = ECSConfig(isHybris = true)
+        setApiKey()
+
+        var ecscallback = object : ECSCallback<ECSShoppingCart, ECSError> {
+            override fun onResponse(result: ECSShoppingCart) {
+                fail()
+            }
+
+            override fun onFailure(ecsError: ECSError) {
+                fail()
+            }
+        };
+
+        var data = Data(null, "id", "type")
+        var eCSShoppingCart = ECSShoppingCart(data)
+        Mockito.`when`(requestHandlerMock.handleRequest(CreateCartRequestMock))
+                .then { ecscallback.onResponse(eCSShoppingCart) }
+        CreateCartRequestMock = CreateCartRequest("ctn", 1, ecscallback)
+        try {
+            mECSCartManager.createECSShoppingCart("ct   n", 1, ecscallback) // invalid CTN
+        } catch (e: ECSException) {
+            assertEquals(ECSErrorType.ECSUnknownIdentifierError.errorCode, e.errorCode)
+        }
+
 
     }
 
     @Test
-    fun `invalid quantity`(){
+    fun `invalid quantity`() {
+        ECSDataHolder.locale = "en_US"
+        ECSDataHolder.config = ECSConfig(isHybris = true)
+        setApiKey()
+
+        var ecscallback = object : ECSCallback<ECSShoppingCart, ECSError> {
+            override fun onResponse(result: ECSShoppingCart) {
+                fail()
+            }
+
+            override fun onFailure(ecsError: ECSError) {
+                fail()
+            }
+        };
+
+        var data = Data(null, "id", "type")
+        var eCSShoppingCart = ECSShoppingCart(data)
+        Mockito.`when`(requestHandlerMock.handleRequest(CreateCartRequestMock))
+                .then { ecscallback.onResponse(eCSShoppingCart) }
+        CreateCartRequestMock = CreateCartRequest("ctn", 0, ecscallback)
+        try {
+            mECSCartManager.createECSShoppingCart("ctn", 0, ecscallback) // invalid Quantity
+        } catch (e: ECSException) {
+            assertEquals(ECSErrorType.ECSCommerceCartModificationError.errorCode, e.errorCode)
+        }
+    }
+
+    @Test
+    fun `invalid ctn and quantity`() {
+        ECSDataHolder.locale = "en_US"
+        ECSDataHolder.config = ECSConfig(isHybris = true)
+        setApiKey()
+
+        var ecscallback = object : ECSCallback<ECSShoppingCart, ECSError> {
+            override fun onResponse(result: ECSShoppingCart) {
+                fail()
+            }
+
+            override fun onFailure(ecsError: ECSError) {
+                fail()
+            }
+        };
+
+        var data = Data(null, "id", "type")
+        var eCSShoppingCart = ECSShoppingCart(data)
+        Mockito.`when`(requestHandlerMock.handleRequest(CreateCartRequestMock))
+                .then { ecscallback.onResponse(eCSShoppingCart) }
+        CreateCartRequestMock = CreateCartRequest("ctn", 0, ecscallback)
+        try {
+            mECSCartManager.createECSShoppingCart("ct   n", 0, ecscallback) // invalid CTN
+        } catch (e: ECSException) {
+            assertEquals(ECSErrorType.ECSUnknownIdentifierError.errorCode, e.errorCode)
+        }
+
+
 
     }
 
