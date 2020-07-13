3083a5ea14d95dbce7ec240a80fbb1522f2516f1
Anurag Gautam <anurag.gautam@philips.com>
2020-07-13 19:18:03 +0530

    Removing  TODO

--- Source/mec/Source/Library/mec/src/main/java/com/philips/platform/mec/screens/detail/EcsProductDetailViewModel.kt
+++ Source/mec/Source/Library/mec/src/main/java/com/philips/platform/mec/screens/detail/EcsProductDetailViewModel.kt
@@ -79,7 +79,7 @@ class EcsProductDetailViewModel : com.philips.platform.mec.common.CommonViewMode
                 addProductToShoppingcart(ecsProductAsParamter,addToProductCallBack)
             }
             override fun onFailure(error: Exception?, ecsError: ECSError?) {
-                TODO(" create cart must NOT fail")
+               // create cart must NOT fail
             }
         }
         ecsProductDetailRepository.createCart(createShoppingCartCallback)
