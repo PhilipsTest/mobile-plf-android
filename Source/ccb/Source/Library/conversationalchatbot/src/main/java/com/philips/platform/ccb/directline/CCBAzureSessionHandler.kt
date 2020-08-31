88744bd23e1c2afb99422135bb67927359ae6e50
pabitra <pabitrakumar.sahoo@philips.com>
2020-08-31 14:31:39 +0530

    Added test cases for CCBAzureSessionHandler

--- Source/ccb/Source/Library/conversationalchatbot/src/main/java/com/philips/platform/ccb/directline/CCBAzureSessionHandler.kt
+++ Source/ccb/Source/Library/conversationalchatbot/src/main/java/com/philips/platform/ccb/directline/CCBAzureSessionHandler.kt
@@ -29,21 +29,25 @@ class CCBAzureSessionHandler : CCBSessionHandlerInterface {
 
     var ccbWebSocketConnection: CCBWebSocketConnection? = null
 
-    private val ccbRestClient by lazy { CCBRestClient() }
+    internal var ccbRestClient = CCBRestClient()
+
+    internal var ccbError = CCBError(CCBErrorCdes.NETWORK_ERROR, "Chatbot Error")
 
     override fun authenticateUser(ccbUser: CCBUser, completionHandler: (Boolean, CCBError?) -> Unit) {
         val ccbAuthenticationRequest = CCBAuthenticationRequest(ccbUser.secretKey)
 
-        ccbRestClient.invokeRequest(ccbAuthenticationRequest, Response.Listener { response: String ->
+        ccbRestClient.invokeRequest(ccbAuthenticationRequest, getSuccessListener(completionHandler), getErrorListener(completionHandler))
+    }
+
+    private fun getSuccessListener(completionHandler: (Boolean, CCBError?) -> Unit): Response.Listener<String> {
+        return Response.Listener { response: String ->
             val tokenObject = JSONObject(response)
             val accessToken = tokenObject.getString("token")
             val conversationId = tokenObject.getString("conversationId")
             CCBManager.token = accessToken
             CCBManager.conversationId = conversationId
             completionHandler.invoke(true, null)
-        }, Response.ErrorListener { error: VolleyError ->
-            completionHandler.invoke(false, CCBError(CCBErrorCdes.NETWORK_ERROR, "Chatbot Error"))
-        })
+        }
     }
 
     override fun startConversation(ccbUser: CCBUser, completionHandler: (Boolean, CCBError?) -> Unit) {
@@ -94,5 +98,11 @@ class CCBAzureSessionHandler : CCBSessionHandlerInterface {
         }, Response.ErrorListener { error: VolleyError ->
             completionHandler.invoke(false, CCBError(CCBErrorCdes.NETWORK_ERROR, "Chatbot Error"))
         })
+    }
+
+    internal fun getErrorListener(completionHandler: (Boolean, CCBError?) -> Unit): Response.ErrorListener {
+        return Response.ErrorListener { error: VolleyError ->
+            completionHandler.invoke(false, ccbError)
+        }
     }
 }
