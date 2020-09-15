73b14ad9c7fd17b5c069dcabecac0114914b4c21
pabitra <pabitrakumar.sahoo@philips.com>
2020-09-15 15:10:28 +0530

    Local ctn check for ECS error code and error type has been changed to ECSPIL_NOT_FOUND_productId in alignment to iOS

--- Source/ecs/Source/Library/philipsecommercesdk/src/test/java/com/philips/platform/ecs/microService/manager/ECSRetailerManagerTest.kt
+++ Source/ecs/Source/Library/philipsecommercesdk/src/test/java/com/philips/platform/ecs/microService/manager/ECSRetailerManagerTest.kt
@@ -64,7 +64,7 @@ class ECSRetailerManagerTest {
         try {
             ecsRetailerManager.fetchRetailers(inValidCTN,ecsCallbackMock)
         }catch (e : ECSException){
-            assertEquals(ECSErrorType.ECSPIL_INVALID_PARAMETER_VALUE_productId.errorCode,e.errorCode)
+            assertEquals(ECSErrorType.ECSPIL_NOT_FOUND_productId.errorCode,e.errorCode)
         }
 
     }
