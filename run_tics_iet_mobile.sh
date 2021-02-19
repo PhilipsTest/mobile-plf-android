28357f4538d5b72a8c8ddb9dd9ad25c5c3ecc870
Abhilash Issac <66210365+abhilashissac@users.noreply.github.com>
2021-02-19 18:27:36 +0530

    Update the newly created mount location for TiCS

--- run_tics_iet_mobile.sh
+++ run_tics_iet_mobile.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if mount | grep "on /mnt/tics" > /dev/null; then
+if mount | grep "on /Volumes/tics" > /dev/null; then
     echo "TICS Share is mounted"
 else
     echo "TICS Share is not mounted"
     echo "Mount TICS Share"
     /mnt/connect_tics_share.sh
 fi
-export TICS=/mnt/tics/configurations/IET-EMS
-export PATH=/mnt/tics/wrappers/macos/Client:${PATH}
-export PATH=${PATH}:/mnt/tics/wrappers/macos/BuildServer
+export TICS=/Volumes/tics/configurations/IET-EMS
+export PATH=/Volumes/tics/wrappers/macos/Client:${PATH}
+export PATH=${PATH}:/Volumes/tics/wrappers/macos/BuildServer
 
 echo "Running TICS..."
-TICSMaintenance -project EMS_Android -branchname feature/serialVersionId -branchdir .
+TICSMaintenance -project EMS_Android -branchname develop -branchdir .
 TICSQServer -project EMS_Android -calc ALL -recalc COMPILERWARNING -nosanity -tmpdir ./EMS_Android
